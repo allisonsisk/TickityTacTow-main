8e184aa55fd9e81f826c7dae7512d714b65c53ed
allison sisk <allisonsisk04@gmail.com>
2022-04-21 11:53:42 -0400

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Jared Davidson on 5/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var ticTacToeModel = TicTacToeModel()
+    @State var gameOver : Bool = false
+    
+    func buttonAction(_ index : Int) {
+        _ = self.ticTacToeModel.makeMove(index: index, player: .home)
+        self.gameOver = self.ticTacToeModel.gameOver.1
+    }
+    
+    var body: some View {
+        VStack {
+            //title
+            Text("Tickity Tac Toe")
+                .bold()
+                .foregroundColor(Color.black.opacity(0.7))
+                .padding(.bottom)
+                .font(.title2)
+            ForEach(0 ..<
+                    
+                    //button squares for game
+                    
+                    ticTacToeModel.squares.count / 3, content: {
+                row in
+                HStack {
+                    ForEach(0 ..< 3, content: {
+                        column in
+                        let index = row * 3 + column
+                        SquareView(dataSource: ticTacToeModel.squares[index], action: {self.buttonAction(index)})
+                    })
+                }
+            })
+        }.alert(isPresented: self.$gameOver, content: {
+            Alert(title: Text("Game Over"),
+                  message:
+                    //explanations for who wins and the resets
+                  Text(self.ticTacToeModel.gameOver.0 != .empty ? self.ticTacToeModel.gameOver.0 == .home ? "You Win!": "AI Wins!" : "Nobody Wins" ) , dismissButton: Alert.Button.destructive(Text("Ok"), action: {
+                    self.ticTacToeModel.resetGame()
+                  }))
+        })
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
